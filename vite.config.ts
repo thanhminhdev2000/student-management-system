e93558d05b16e3f14924c67f61ebb0526e5371dd
thanhminhdev2000 <69253365+thanhminhdev2000@users.noreply.github.com>
2025-08-31 08:30:15 +0700

    Setup github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/student-management-system/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
