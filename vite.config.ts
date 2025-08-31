6069b06c02a67991ce050e5112d20588d18f8e6a
thanhminhdev2000 <69253365+thanhminhdev2000@users.noreply.github.com>
2025-09-01 06:28:12 +0700

    Setting deploy on github

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: '/student-management-system/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
